c3a3b1389c30533119b736e4574c2cc74021099b
MichaelHYJiang <450199156@qq.com>
2017-05-20 15:39:56 +0800

    Update svmTraining.py

--- RP using Edge and Color/svmTraining.py
+++ RP using Edge and Color/svmTraining.py
@@ -1,64 +1,67 @@
-# -*- coding: cp936 -*-
+#!/usr/bin/python
+# -*- coding: uft-8 -*-
+
+# Using sampled pictures to train a SVM
+# based on file svm.py, which came from the book 'Machine Learning in Action'
+
 import svm
 import cv2
+import os
 import numpy as np
 import time
 
-def loadimgdata():
-    n_pic_pos = 503
-    n_pic_neg = 11453
+# Number of all positive samples
+POS = 503
+# Number of all negative samples
+NEG = 11453
+
+# function 'loadimgdata' reads all positive and negative samples into a variable named 'data'
+# along with a variable named 'label' to denote the category of each sample vector
+def loadimgdata(quick = True,   # when enabled, the number of negative samples will be contorled in 2 to 3 times of the positive ones
+                width = 20,
+                height = 5,
+                channel = 3):   # width, height and channel of the region that samples will be resized to, their multiplication is the size of each data vector
+    
+    n_pic_pos = POS
+    n_pic_neg = NEG
     label = []
     data = []
-    for i in range(n_pic_pos):
-        img = cv2.imread('H:\\³µÅÆÍ¼Ïñ\\Feature Training\\POS\\'+str(i)+'.jpg')
-        img = cv2.resize(img, (20,5),interpolation = cv2.INTER_CUBIC)
-        s = []
-        for i0 in range(5):
-            for i1 in range(20):
-                for i2 in range(3):
-                    s.append(img[i0,i1,i2])
-        data.append(s)
-        label.append(1.0)
-    for i in range(n_pic_neg):
-        img = cv2.imread('H:\\³µÅÆÍ¼Ïñ\\Feature Training\\negative\\'+str(i)+'.jpg')
-        img = cv2.resize(img, (20,5),interpolation = cv2.INTER_CUBIC)
-        s = []
-        for i0 in range(5):
-            for i1 in range(20):
-                for i2 in range(3):
-                    s.append(img[i0,i1,i2])
-        data.append(s)
-        label.append(-1.0)
+    t = time.time()
+    n0 = 0
+    for i in range( n_pic_pos):
+        filepath = os.path.join( '.', 'Feature Training', 'POS', str( i) + '.jpg')
+        img = cv2.imread( filepath)
+        img = cv2.resize( img, ( width, height), interpolation = cv2.INTER_CUBIC)
+        if channel != 3:
+            img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        s = img.reshape(( width * height * channel,))
+        data.append( s)
+        label.append( 1.0)
+        n0 += 1
+    print 'load %d positive samples' % n0
+    if quick:
+        p = n_pic_pos * 2.5 / float(n_pic_neg)
+    else:
+        p = 1.0
+    n0 = 0
+    for i in range( n_pic_neg):
+        if np.random.rand() > p:
+            continue
+        filepath = os.path.join( '.', 'Feature Training', 'negative', str( i) + '.jpg')
+        img = cv2.imread( filepath)
+        img = cv2.resize( img, ( width, height), interpolation = cv2.INTER_CUBIC)
+        if channel != 3:
+            img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        s = img.reshape((width * height * channel,))
+        data.append( s)
+        label.append( -1.0)
+        n0 += 1
+    print 'load %d negative samples' % n0
+    print 'total elapsed time: %.2f s' % (time.time() - t)
+        
     return data, label
 
-def loadimg():
-    n_pic_pos = 503
-    n_pic_neg = 11453
-    label = []
-    data = []
-    wid = 30
-    hei = 10
-    for i in range(n_pic_pos):
-        img = cv2.imread('H:\\³µÅÆÍ¼Ïñ\\Feature Training\\POS\\'+str(i)+'.jpg')
-        if img.ndim == 3:
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        img = cv2.resize(img, (wid, hei),interpolation = cv2.INTER_CUBIC)
-        img = img.reshape(1, wid * hei)
-        data.append(img)
-        label.append(1.0)
-    for i in range(n_pic_neg):
-        if np.random.rand() < 0.95:
-            continue
-        img = cv2.imread('H:\\³µÅÆÍ¼Ïñ\\Feature Training\\negative\\'+str(i)+'.jpg')
-        if img.ndim == 3:
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        img = cv2.resize(img, (wid, hei),interpolation = cv2.INTER_CUBIC)
-        img = img.reshape(1, wid * hei)
-        data.append(img)
-        label.append(-1.0)
-    return data, label
-def training(d, l, C = 0.6, tol = 0.01, max_iter = 50, p = True):
-    # d, l = loadimgdata()
+def training(d, l, C = 0.6, tol = 0.01, max_iter = 20, p = True):
     b, alphas = svm.smoP(d, l, C, tol, max_iter)
     if p == True:
         print calcrate(d, l, b, alphas)
@@ -81,7 +84,7 @@ def calcrate(d, l, b, alphas, n_pos = 503):
     r2 = sum(x0[0:n_pos]) / 2.0 / n_pos
     r3 = sum(x0[n_pos:]) / 2.0 / (n - n_pos)
     return r1,r2,r3
-    #print 'total:%f\npositive:%f\nnegative:%f\n' % (r1, r2, r3)
+    print 'total:%f\npositive:%f\nnegative:%f\n' % (r1, r2, r3)
 
 def quicktraining(C0 = 0.6):
     n_pic_pos = 503
@@ -90,26 +93,7 @@ def quicktraining(C0 = 0.6):
     data = []
     width = 36
     height = 12
-    for i in range(n_pic_pos):
-        img = cv2.imread('H:\\³µÅÆÍ¼Ïñ\\Feature Training\\POS\\'+str(i)+'.jpg',0)
-        img = cv2.resize(img, (width, height),interpolation = cv2.INTER_CUBIC)
-        s = []
-        for i0 in range(height):
-            for i1 in range(width):
-                s.append(img[i0,i1])
-        data.append(s)
-        label.append(1.0)
-    for i in range(n_pic_neg):
-        if np.random.rand() < 0.97:
-            continue
-        img = cv2.imread('H:\\³µÅÆÍ¼Ïñ\\Feature Training\\negative\\'+str(i)+'.jpg',0)
-        img = cv2.resize(img, (width, height),interpolation = cv2.INTER_CUBIC)
-        s = []
-        for i0 in range(height):
-            for i1 in range(width):
-                s.append(img[i0,i1])
-        data.append(s)
-        label.append(-1.0)
+    [data, label] = loadimgdata(width = width, height = height, channel = 1)
     print len(label)
     print 'strat training'
     t0 = time.time()
